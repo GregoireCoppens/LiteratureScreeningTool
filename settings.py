b64432278e4696592fb9947d1e251bb5bbc179e1
Grégoire Coppens <gregoire.coppens@gmail.com>
2018-09-29 13:52:16 +0200

    uploaded settings file

---
+++ settings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Pubmed Requirements:
+mail = '###'
+pubmed_searchterm = '"Medical Informatics"[Mesh]'
+
+# Web Of Science Requirements:
+wos_auth_search = '###'
+wos_auth_retrieve = '###'
+wos_username = '###'
+wos_pass = '###'
+wos_query = 'TS=("Medical Informatics")'
+
+# SCOPUS Requirements:
+SCOPUS_apikey = '###'
+SCOPUS_institutionkey = '###'
+SCOPUS_query = 'TITLE-ABS-KEY ("Medical Informatics")'
+
+# LST Requirements:
+Article_Table = "Article_Table.csv"
+screeners = "test, experiment"
+filters_all = ["criteria 1", "criteria 2", "criteria 3", "criteria 4", "criteria 5", "criteria 6", "criteria 7"]
